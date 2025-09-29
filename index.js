a7858fd7524107b8eebec56b80b64c4bd749f2a8
Md fahad <mohammadfahad572000@gmail.com>
2025-09-30 00:33:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+var randomNumber1=Math.random();
+randomNumber1=(randomNumber1*6)+1;
+randomNumber1=Math.floor(randomNumber1);
+  
+
+var randomDice="images/"+"dice"+randomNumber1+".png";
+var img1=document.querySelectorAll("img")[0];
+img1.setAttribute("src",randomDice);
+
+
+
+var randomNumber2=Math.random();
+randomNumber2=(randomNumber2*6)+1;
+randomNumber2=Math.floor(randomNumber2);
+  
+
+var randomDice1="images/"+"dice"+randomNumber2+".png";
+var img2=document.querySelectorAll("img")[1];
+img2.setAttribute("src",randomDice1);
+
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="player  1 wins";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="player 2 wins";
+}
+else{
+     document.querySelector("h1").innerHTML="Draw";
+}
+
+
+
+
+
+
+
+
